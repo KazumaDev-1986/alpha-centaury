e1acb94b8cb3603498c72953aa6f38ce1ac09598
Kazuma Dev <kazuma.dev1986@outlook.com>
2025-07-19 19:49:55 +0200

    feat: Roadmap file added

---
+++ src/include/level.h
@@ -0,0 +1,19 @@
+#ifndef AC_LEVEL_H
+#define AC_LEVEL_H
+
+#include "config.h"
+
+AC typedef struct {
+  // TODO
+} Level;
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // AC_LEVEL_H
